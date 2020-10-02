ba2297b2710715c398b49ac3cb2b95c4e11f78fa
jackyeagler <72009737+jackyeagler@users.noreply.github.com>
2020-10-02 17:20:11 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+	<meta charset="UTF-8">
+	<title> MTE</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+		<div class="logo">
+			<div id="lineLong" class="line"></div>
+			<div id="lineShort"class="line"></div>
+			<div id="lineMedium"class="line"></div>		
+		</div>
+	
+	<div class="carousel1">
+		<div id="content1">THE</div>
+		<div id="content2">STAGE IS</div>
+	<div class="content3"><span>W A I T I N G.</span></div>	
+	<div class="carousel2">	
+	<div class="carousel3">
+	<div class="carousel4">
+		<h2>About Us</h2>
+				<p> The Maskara Theatre Ensemble was conceived in La Salle College when it was founded by Peque Gallaga in 1972. 
+				The organization which used to be called “Crossing Kamatis Rapapap Kutsi-Kutsi Society” then changed to “The La Salle Theatre Guild” was created to enhance the cultural development among its members. In 1973, the group bore the name “Maskara Theatre Ensemble”,
+				the first word which had been derived from the a logo representing the Igorot symbol for fetility, designed by graphic 
+				designer Franco Patriarca.Maskara had become an outlet for people who love to do theatre work and who believe in the magic of theatre.
+				It serves to cultivate the foundation of theatre in entertainment, education and advocacy through the extensive and rigorous training 
+				it provides its members; and the varied productions it represents to the community. 
+				</p>
+	<div class="carousel5">Read More</div>
+
+	</div>
+	</div>
+	</div>
+	</div>
+	<div class="footer">
+		<div id="footer2">Maskara</div>
+		<div id="footer3">Theatre</div>
+		<div id="footer4">Ensemble</div>
+		
+	<div class="footer1">
+			<div class="content4">Telephone:</div>
+			<div class="content5">Address:</div>
+			<div class="content6">University of St. La Salle</div>
+			<div class="content6">Yanson Building, 3rd Floor</div>
+			<div class="content6">La Salle Avenue St., Bacolod City </div>
+			<div class="content6">444 2056 </div>
+			<div class="content6">09123456789 </div>
+		</div>
+	<div class="icons">
+	<a href="#" class="fa fa-facebook"></a>
+	<a href="#" class="fa fa-instagram"></a>	
+	<a href="#" class="fa fa-google"></a>
+		</div>
+		</div>
+		</div>
+		</div>
+</body>
+</html>
